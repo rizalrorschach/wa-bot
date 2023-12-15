8f01b6e9a885d651f70229020ee29f2ab159f7d3
Rizal Rorschach <rizalrorschach@gmail.com>
2023-12-15 17:59:28 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const qrcode = require("qrcode-terminal");
+
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message", async (message) => {
+  if (message.body === "!ping") {
+    client.sendMessage(message.from, "pong");
+  } else if ((message.type === "image", "video" && message.body === ".sticker")) {
+    const media = await message.downloadMedia();
+
+    client.sendMessage(message.from, media, {
+      sendMediaAsSticker: true,
+      stickerName: "Rorschach",
+      stickerAuthor: "Rorschach BOT",
+    });
+  }
+});
+
+// Mention everyone
+client.on("message", async (msg) => {
+  if (msg.body === "!everyone") {
+    const chat = await msg.getChat();
+
+    let text = "";
+    let mentions = [];
+
+    for (let participant of chat.participants) {
+      const contact = await client.getContactById(participant.id._serialized);
+
+      mentions.push(contact);
+      text += `@${participant.id.user} `;
+    }
+
+    await chat.sendMessage(text, { mentions });
+  }
+});
+
+client.initialize();
---
+++ readme.md
@@ -0,0 +1,28 @@
+# WhatsApp Bot
+
+This is a simple WhatsApp bot built using the `whatsapp-web.js` library.
+
+## Features
+
+- Responds to a "!everyone" command by mentioning all participants in a chat.
+- Create stickers from "Image" and "Video"
+
+## Setup
+
+1. Clone this repository.
+2. Run `npm install` to install the required dependencies.
+3. Run `node index.js` to start the bot.
+
+## Usage
+
+Send a "!everyone" message in a chat where the bot is a participant to have the bot mention all participants.
+
+Send a ".stickers" message to create stickers
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
